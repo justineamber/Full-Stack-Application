9f87395dce9c9625494420d7d40c83c3691b9e60
Justine Hill <justinehill@Justines-MacBook-Pro.local>
2020-07-16 13:18:03 +0200

    Comitting updates to Front End

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,9 +1,62 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import { defaultState } from "../../server/defaultState";
+import { createLogger } from "redux-logger";
+import createSagaMiddleware from "redux-saga";
 
-export const store = createStore(function reducer(
-  state = defaultState,
-  action
-) {
-  return state;
-});
+const sagaMiddleware = createSagaMiddleware();
+import * as sagas from "./sagas.mock";
+import * as mutations from "./mutations";
+import { take } from "redux-saga/effects";
+
+export const store = createStore(
+  combineReducers({
+    tasks(tasks = defaultState.tasks, action) {
+      switch (action.type) {
+        case mutations.CREATE_TASK:
+          return [
+            ...tasks,
+            {
+              id: action.taskID,
+              name: "New task",
+              group: action.groupID,
+              owner: action.ownerID,
+              isComplete: false
+            }
+          ];
+        case mutations.SET_TASK_COMPLETE:
+          return tasks.map(tasks => {
+            return task.id === action.taskID
+              ? { ...task, isComplete: action.isComplete }
+              : task;
+          });
+        case mutations.SET_TASK_NAME:
+          return tasks.map(tasks => {
+            return task.id === action.taskID
+              ? { ...task, name: action.name }
+              : task;
+          });
+        case mutations.SET_TASK_GROUP:
+          return tasks.map(tasks => {
+            return task.id === action.taskID
+              ? { ...task, group: action.groupID }
+              : task;
+          });
+      }
+      return tasks;
+    },
+    comments(comments = defaultState.comments) {
+      return comments;
+    },
+    groups(groups = defaultState.groups) {
+      return groups;
+    },
+    users(users = defaultState.users) {
+      return users;
+    }
+  }),
+  applyMiddleware(createLogger(), sagaMiddleware)
+);
+
+for (let saga in sagas) {
+  sagaMiddleware.run();
+}
